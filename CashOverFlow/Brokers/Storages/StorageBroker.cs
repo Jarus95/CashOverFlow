66303f2655ce8394ff2f1ba371bba8125e736d2b
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-13 17:29:21 +0500

    CONFIGS: Registr DbContext

---
+++ CashOverFlow/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+
+using CashOverFlow.Models.Locations;
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CashOverFlow.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionstring = "Server=(localdb)\\MSSQLLocalDB;Database=CashOverflowDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+    }
+}
+
+
+//this.configuration.GetConnectionString(name: "DefaultConnection");
---
+++ CashOverFlow/Migrations/20230813121705_Create-Locations-table.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashOverFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateLocationstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Counrty = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
