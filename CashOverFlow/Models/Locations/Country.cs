46a3007b847bf2daf4aa6cecf7ff8c146427098f
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-13 17:30:01 +0500

    DATA:  Add location Models

---
+++ CashOverFlow/Models/Locations/Country.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+
+namespace CashOverFlow.Models.Locations
+{
+    public enum Country
+    {
+        Uzbekistan
+    }
+}
---
+++ CashOverFlow/Models/Locations/Location.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+
+namespace CashOverFlow.Models.Locations
+{
+    public class Location
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+        public Country Counrty { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
