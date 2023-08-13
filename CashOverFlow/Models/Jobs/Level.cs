9348120fade99ea1c1ec955ff6afa089e389f40b
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-08-14 00:20:33 +0900

    DATA: Add Jobs Model

---
+++ CashOverFlow/Models/Jobs/Level.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+
+namespace CashOverFlow.Models.Jobs
+{
+    public enum Level
+    {
+        Others,
+        Junior,
+        Middle,
+        Senior
+    }
+}
