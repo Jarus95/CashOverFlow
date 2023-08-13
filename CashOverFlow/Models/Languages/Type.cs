2b3b6c7d5ae5bde66c4fcb6432ff266120c7cb3b
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-08-14 00:46:43 +0900

    DATA: Create Language Model

---
+++ CashOverFlow/Models/Languages/Type.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+
+namespace CashOverFlow.Models.Languages
+{
+    public enum Type
+    {
+        Others,
+        Frontend,
+        Backend,
+        Fullstack
+    }
+}
