06fafc0750d3bc61a9daf4469e7c5d68116aa4e5
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-14 08:09:04 +0500

    Create Salary Table

---
+++ CashOverFlow/Brokers/Storages/StorageBroker.Salaries.cs
@@ -0,0 +1,10 @@
+﻿using CashOverFlow.Models.Salaries;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashOverFlow.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        DbSet<Salary> Salaries { get; set; }
+    }
+}
