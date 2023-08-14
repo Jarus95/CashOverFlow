0abaf763836d241602f1f1fc809a8ee5384ae149
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-14 08:08:28 +0500

    Create Salary Model

---
+++ CashOverFlow/Models/Salaries/Salary.cs
@@ -0,0 +1,13 @@
+﻿namespace CashOverFlow.Models.Salaries
+{
+    public class Salary
+    {
+        public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+        public int Experience { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public Guid LocationId { get; set; }
+        public Guid LanguageId { get; set; }
+        public Guid JobId { get; set; }
+    }
+}
